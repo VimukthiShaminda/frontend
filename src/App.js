6ace065baa86a128529d7d4488e974463f6f5603
vimukthi871 <vimukthishaminda871@gmail.com>
2023-01-12 02:45:41 +0530

    just start

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import {BrowserRouter, Router, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <div className="pages">
       
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
